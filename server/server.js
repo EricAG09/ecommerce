5e0adc6836f1421e327119a754bcc2e820d4e3f4
EricAG09 <ericagoficial@gmail.com>
2024-10-01 18:33:26 -0300

    conectado server com bd e isntalado algumas dependencias do tailwind, Erro no jsconfig

---
+++ server/server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+
+mongoose
+    .connect('mongodb+srv://eric:ericag@cluster0.2mf7f.mongodb.net/')
+        .then(() => console.log('Conectado'))
+        .catch(error => console.log(error));
+
+const app = express()
+const PORT = process.env.PORT || 5000;
+
+app.use(
+    cors({
+        origin: 'http://localhost:5173/',
+        methods: ['GET', 'POST', 'DELETE', 'PUT'],
+        allowedHeaders: [
+            "Content-Type",
+            'Authoriation',
+            'Cache-Control',
+            'Expires',
+            'Pragma'
+        ],
+        credentials: true
+    })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`));
